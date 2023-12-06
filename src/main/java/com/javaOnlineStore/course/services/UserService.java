7ce3c50d9e416c61b70af6b913a9db8c8da83fa9
Lucas c. Marinho <lucasc.marinho@hotmail.com>
2023-12-06 15:54:06 -0300

    exception handling - update

--- src/main/java/com/javaOnlineStore/course/services/UserService.java
+++ src/main/java/com/javaOnlineStore/course/services/UserService.java
@@ -12,40 +12,46 @@ import com.javaOnlineStore.course.entities.User;
 import com.javaOnlineStore.course.repositories.UserRepository;
 import com.javaOnlineStore.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		} catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			e.getMessage();
 		}
-		
+
 	}
-	
+
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
+		updateData(entity, obj);	
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
